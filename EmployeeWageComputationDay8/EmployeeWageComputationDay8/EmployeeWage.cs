12c1611bca3d0b29fd072fd93d42033d7de7ad20
Rahullal <Rahullal432@gmail.com>
2023-05-09 22:46:23 +0530

    Calculate Daily Employee Wage

--- EmployeeWageComputationDay8/EmployeeWageComputationDay8/EmployeeWage.cs
+++ EmployeeWageComputationDay8/EmployeeWageComputationDay8/EmployeeWage.cs
@@ -10,30 +10,33 @@ namespace EmployeeWageComputationDay8
     {
         public void EmployeeAttendence()
         {
+            int EMP_RATE_PER_HOUR = 20;
 
-                int EMP_RATE_PER_HOUR = 20;
-
-                int IS_FULL_TIME = 1;
-
-                int empHours = 0;
-                int empWages = 0;
-                Random rdm = new Random();
-                int empCheck = rdm.Next(2);
-                if (empCheck == IS_FULL_TIME)
-                {
-                    empHours = 8;
-                }
-                else
-                {
-                    empHours = 0;
-                }
-                empWages = empHours * EMP_RATE_PER_HOUR;
-                Console.WriteLine("Total wage of the employee :" + empWages);
-
-
-
+            int IS_FULL_TIME = 1;
+            int IS_PART_TIME = 2;
+            int empHours = 0;
+            int empWages = 0;
+            Random rdm = new Random();
+            int empCheck = rdm.Next(3);
+            if (empCheck == IS_FULL_TIME)
+            {
+                empHours = 8;
             }
+            else if (empCheck == IS_PART_TIME)
+            {
+                empHours = 4;
+            }
+            else
+            {
+                empHours = 0;
+            }
+            empWages = empHours * EMP_RATE_PER_HOUR;
+            Console.WriteLine("Total wage of the employee :" + empWages);
+
+
 
         }
+
     }
+}
 
