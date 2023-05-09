167fe43bf6b2792b65420cf9e7b7ed9e364d1579
Rahullal <Rahullal432@gmail.com>
2023-05-09 22:51:25 +0530

    Calculate Daily Employee Wage

--- EmployeeWageComputationDay8/EmployeeWageComputationDay8/EmployeeWage.cs
+++ EmployeeWageComputationDay8/EmployeeWageComputationDay8/EmployeeWage.cs
@@ -10,37 +10,39 @@ namespace EmployeeWageComputationDay8
     {
         public void EmployeeAttendence()
         {
-            int EMP_RATE_PER_HOUR = 20;
-
-            int IS_FULL_TIME = 1;
-            int IS_PART_TIME = 2;
+            const int EMP_RATE_PER_HOUR = 20;
+            const int IS_FULL_TIME = 1;
+            const int IS_PART_TIME = 2;
+            const int NUM_OF_WORKING_DAYS = 5;
             int empHours = 0;
             int empWages = 0;
-            Random rdm = new Random();
-            int empCheck = rdm.Next(3);
+            int totalEmpwages = 0;
+            for (int day = 0; day < NUM_OF_WORKING_DAYS; day++)
             {
+
+                Random rdm = new Random();
+                int empCheck = rdm.Next(3);
                 switch (empCheck)
                 {
-                    case 0:
-                        empCheck = 0;
-                        empHours = 0;
-                        break;
-                    case 1:
-                        empCheck = 1;
+                    case IS_FULL_TIME:
                         empHours = 8;
                         break;
-                    case 2:
-                        empCheck = 2;
+                    case IS_PART_TIME:
                         empHours = 4;
+                        break;
+                    default:
+                        empHours = 0;
                         break;
                 }
 
+
+                empWages = empHours * EMP_RATE_PER_HOUR;
+                Console.WriteLine("Total wage of the employee :" + empWages);
+                totalEmpwages += empWages;
             }
-            empWages = empHours * EMP_RATE_PER_HOUR;
-            Console.WriteLine("Total wage of the employee :" + empWages);
-
+            Console.WriteLine("total EmpWages : " + totalEmpwages);
         }
-
     }
+
 }
 
