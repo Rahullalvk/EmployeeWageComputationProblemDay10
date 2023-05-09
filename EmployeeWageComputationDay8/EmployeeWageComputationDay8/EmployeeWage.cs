ad934f05a6b3978e3f945b0f2b26cd3ae50ed4e3
Rahullal <Rahullal432@gmail.com>
2023-05-09 22:37:07 +0530

    Check Employee is Present or Absent

---
+++ EmployeeWageComputationDay8/EmployeeWageComputationDay8/EmployeeWage.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageComputationDay8
+{
+    internal class EmployeeWage
+    {
+        public void EmployeeAttendence()
+        {
+
+            int IS_FULL_TIME = 1;
+            Random rdm = new Random();
+            int empCheck = rdm.Next(2);
+            if (empCheck == IS_FULL_TIME)
+            {
+                Console.WriteLine("Employee Is present");
+            }
+            else
+            {
+                Console.WriteLine("Employee Is absent");
+            }
+
+        }
+    }
+}
--- EmployeeWageComputationDay8/EmployeeWageComputationDay8/Program.cs
+++ EmployeeWageComputationDay8/EmployeeWageComputationDay8/Program.cs
@@ -1,10 +1,14 @@
-﻿namespace EmployeeWageComputationDy8 
+﻿using EmployeeWageComputationDay8;
+
+namespace EmployeeWageComputationDy8 
 {
     class program 
     {
         static void Main(string[] args) 
         {
             Console.WriteLine("Welcome to Employee Wage Computation Program");
+            EmployeeWage employeeWage = new EmployeeWage();
+            employeeWage.EmployeeAttendence();
         }
     }
 }
